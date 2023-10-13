f3eadf6781fca8646ccd21c624321454e6d71546
Julia Fortna <jmf363@cornell.edu>
2023-10-13 15:45:47 -0400

    make baby pufferfish and shark

--- src/game.ml
+++ src/game.ml
@@ -29,12 +29,54 @@ module MarineAnimal = struct
       age = 0;
       stage = Baby;
     }
+  
+  (**Creates a baby pufferfish*)
+  let pufferfish (name : string) : fish =
+    {
+      name;
+      species = Pufferfish;
+      size = Medium;
+      color = "yellow";
+      food = Algae;
+      hp = 100;
+      hunger = true;
+      age = 0;
+      stage = Baby;
+    }
+  (**Creates a baby shark*)
+  let shark (name : string) : fish =
+    {
+      name;
+      species = Shark;
+      size = Large;
+      color = "grey";
+      food = Fish;
+      hp = 100;
+      hunger = true;
+      age = 0;
+      stage = Baby;
+    }
 end
 
 module Tanks = struct
   type tank_type = Nursery | AdultTank
-  type tank = { tank_type : tank_type; fish_list : MarineAnimal.fish list }
+  type tank = { 
+    tank_type : tank_type; 
+    fish_list : MarineAnimal.fish list; 
+    capacity : int 
+    }
 
-  (**Given a tank type, creates an empty tank*)
-  let new_tank (t : tank_type) : tank = { tank_type = t; fish_list = [] }
+  (** Given a tank type, creates an empty tank*)
+  let new_tank (t : tank_type) : tank = { 
+    tank_type = t; 
+    fish_list = []; 
+    capacity = 10; 
+    }
+    
+   (** Given a tank and a fish, adds the fish to the tank*)
+   let add_fish (f:MarineAnimal.fish) (t:tank) : tank = {
+    tank_type = t.tank_type; 
+    fish_list = f::t.fish_list; 
+    capacity = t.capacity - 1;
+   }
 end
