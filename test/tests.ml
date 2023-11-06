f741875b58d40a3c53a859f11b19921182421321
Julia Fortna <jmf363@cornell.edu>
2023-11-05 19:48:07 -0500

    OUnit test outline

---
+++ test/tests.ml
@@ -0,0 +1,14 @@
+(** Test Plan: The first portion of our test suite is made of up OUnit tests, 
+    *)
+
+open OUnit2
+open Final_project
+
+let fish_tests = []
+let tank_tests = []
+let game_tests = []
+
+let suite =
+  "test suite" >::: List.flatten [ fish_tests; tank_tests; game_tests ]
+
+let () = run_test_tt_main suite
