275bcf93fafa5a882d9149279c65a528515ee993
Julia Fortna <jmf363@cornell.edu>
2023-11-09 17:44:10 -0500

    update output types

--- lib/game.mli
+++ lib/game.mli
@@ -1,22 +1,28 @@
+type fish_species
+type fish_food
 type fish
+
+val species_to_string : fish -> string
+val num_species : int
+
 type tank
 type player_state
 
-val new_tank : fish list -> tank
-val add_fish : fish -> tank -> tank
-val age_fish : fish -> fish
-val age_list : fish list -> fish list
-val age_tank : tank -> tank
-val health_fish : fish -> int -> fish
-val health_list : fish list -> int -> fish list
-val health_tank : tank -> int -> tank
+val make_fish : fish_species -> fish_food -> fish
+val set_tank : tank -> unit
+val fish_pos : fish_species -> int
+val age_fish : fish -> unit
+val age_tank : tank -> unit
+val health_fish : fish -> int -> unit
+val health_tank : tank -> fish_species -> int -> unit
 val start_game : int -> player_state
-val add_fish_game : player_state -> fish -> player_state
+val set_game : player_state -> unit
+val add_fish : fish -> int -> unit
+val add_fish_tank : tank -> fish_species -> int -> unit
+val add_fish_game : player_state -> fish_species -> int -> unit
+val end_of_round : player_state -> unit
+val print_fish : fish -> string
+val print_playermoney : player_state -> int
 val start_round_print : player_state -> string
 val end_round_print : player_state -> string
-val cost : player_state -> int -> player_state
-val end_of_round : player_state -> player_state
-val print_fish : fish -> string
-val print_fish_list : fish list -> string
-val print_tank : tank -> string
-val print_playermoney : player_state -> int
+val cost : player_state -> int -> unit
